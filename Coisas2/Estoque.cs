8d24d8cb4e1c7756c7fab190c330550e4072a130
Luizarealeao <luiz.silva1770@etec.sp.gov.br>
2024-11-24 01:14:06 -0300

    Adicionei coisas.

---
+++ Coisas2/Estoque.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace Vai_porfavor
+{
+    public class EstoqueOperations
+    {
+        private MySqlConnection connection;
+
+        public EstoqueOperations(DatabaseConnection dbConnection)
+        {
+            connection = dbConnection.GetConnection();
+        }
+
+        public List<Produto> GetProdutos()
+        {
+            List<Produto> produtos = new List<Produto>();
+            string query = "SELECT id_Produto, Nome_do_Produto, Numero_do_Lote, Custo_Unitario, Unidade, DataValidade FROM Produto ORDER BY DataValidade ASC";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Produto produto = new Produto
+                {
+                    Id = Convert.ToInt32(reader["id_Produto"]),
+                    Nome = reader["Nome_do_Produto"].ToString(),
+                    Lote = Convert.ToInt32(reader["Numero_do_Lote"]),
+                    CustoUnitario = Convert.ToDecimal(reader["Custo_Unitario"]),
+                    Unidade = Convert.ToInt32(reader["Unidade"]),
+                    DataValidade = Convert.ToDateTime(reader["DataValidade"])
+                };
+                produtos.Add(produto);
+            }
+            reader.Close();
+
+            produtos.RemoveAll(prod => prod.DataValidade < DateTime.Now);
+            return produtos;
+        }
+
+        public decimal CalcularCustoTotal(Produto produto)
+        {
+            return produto.CustoUnitario * produto.Unidade;
+        }
+    }
+}
---
+++ Coisas2/Program.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace Vai_porfavor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conexao = null;
+
+            try
+            {
+                
+                string stringconexao = "server=localhost;uid=root;pwd=1234;database=Projeto";
+                DatabaseConnection dbConnection = new DatabaseConnection(stringconexao);
+
+                dbConnection.OpenConnection();  
+
+                
+                RHOperations rhOps = new RHOperations(dbConnection);
+                EstoqueOperations estoqueOps = new EstoqueOperations(dbConnection);
+
+                
+                List<Funcionario> funcionarios = rhOps.GetFuncionarios();
+                foreach (var func in funcionarios)
+                {
+                    
+                    decimal inss = rhOps.CalcularINSS(func.Salário);
+                    decimal fgts = rhOps.CalcularFGTS(func.Salário);
+                    int tempoDeCasa = rhOps.CalcularTempoDeCasa(func.DataAdmissao);
+
+                    
+                    Console.WriteLine($"Nome: {func.Nome}, CPF: {func.CPF}, Cargo: {func.Cargo}, Salário: {func.Salário:C}, INSS: {inss:C}, FGTS: {fgts:C}, Tempo de Casa: {tempoDeCasa} anos");
+                }
+
+                
+                List<Produto> produtos = estoqueOps.GetProdutos();
+                foreach (var produto in produtos)
+                {
+                    
+                    decimal custoTotal = estoqueOps.CalcularCustoTotal(produto);
+                    Console.WriteLine($"Produto: {produto.Nome}, Lote: {produto.Lote}, Custo Unitário: {produto.CustoUnitario:C}, Custo Total: {custoTotal:C}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            finally
+            {
+                
+                if (conexao != null && conexao.State == System.Data.ConnectionState.Open)
+                {
+                    conexao.Close();
+                    Console.WriteLine("Conexão fechada.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
